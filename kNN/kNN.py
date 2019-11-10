2a3f0d06cbdb53816b2496d352e6472986c773ce
liqsdev <35447978+liqsdev@users.noreply.github.com>
2019-11-10 14:58:36 +0800

    add kNN

---
+++ kNN/kNN.py
@@ -0,0 +1,43 @@
+"""
+k近邻算法
+"""
+
+import numpy as np
+import operator
+
+
+def create_dataset():
+    group = np.array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
+    labels = ['A', 'A', 'B', 'B']
+    return group, labels
+
+
+def classify(in_x, dataset, labels, k):
+    dataset_size = dataset.shape[0]
+    diff_mat = np.tile(in_x, (dataset_size, 1)) - dataset
+    sq_diff_mat = diff_mat**2
+    sq_distances = sq_diff_mat.sum(axis=1)
+    distances = sq_distances**0.5
+    sorted_dist_indices = distances.argsort()
+
+    class_count = {}
+    for i in range(k):
+        vote_label = labels[sorted_dist_indices[i]]
+        class_count[vote_label] = class_count.get(vote_label, 0) + 1
+    sorted_class_count = sorted(class_count.items(), key=operator.itemgetter(1), reverse=True)
+    return sorted_class_count[0][0]
+
+
+if __name__ == '__main__':
+    group, labels = create_dataset()
+    print(classify([1, 1.2], group, labels, 3))
+
+
+
+
+
+
+
+
+
+
